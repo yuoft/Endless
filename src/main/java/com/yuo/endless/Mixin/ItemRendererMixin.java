9dea07d3f825d5fee6ff88d0f72a2308002a8f44
yuo <2685245554@qq.com>
2024-08-21 10:35:40 +0800

    修改mixin注入

--- src/main/java/com/yuo/endless/Mixin/ItemRendererMixin.java
+++ src/main/java/com/yuo/endless/Mixin/ItemRendererMixin.java
@@ -1,15 +1,12 @@
 package com.yuo.endless.Mixin;
 
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
@@ -19,12 +16,28 @@ public abstract class ItemRendererMixin {
      * @author yuo
      * @reason 修改无尽箱子中的物品数量显示
      */
-    @ModifyArgs(method = "renderItemOverlayIntoGUI", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/FontRenderer;renderString(Ljava/lang/String;FFIZLnet/minecraft/util/math/vector/Matrix4f;Lnet/minecraft/client/renderer/IRenderTypeBuffer;ZII)I"))
-    public void renderItemOverlayIntoGUI(Args args, FontRenderer renderer, ItemStack stack, int x, int y, String name){
-        String count = endless$getSimplifiedCount(stack.getCount());
-        args.set(0, count);
-        args.set(1, (float)(x + 19 - 2 - renderer.getStringWidth(count)));
+//    @ModifyArgs(method = "renderItemOverlayIntoGUI", at = @At(value = "INVOKE",
+//            target = "Lnet/minecraft/client/gui/FontRenderer;renderString(Ljava/lang/String;FFIZLnet/minecraft/util/math/vector/Matrix4f;Lnet/minecraft/client/renderer/IRenderTypeBuffer;ZII)I"))
+//    public void renderItemOverlayIntoGUI(Args args, FontRenderer renderer, ItemStack stack, int x, int y, String name){
+//        String count = endless$getSimplifiedCount(stack.getCount());
+//        args.set(0, count);
+//        args.set(1, (float)(x + 19 - 2 - renderer.getStringWidth(count)));
+//    }
+
+//    @Inject(method = "renderItemOverlayIntoGUI",
+//            at = @At(value = "INVOKE",
+//            target = "Ljava/lang/String;valueOf(I)Ljava/lang/String;",
+//            shift = Shift.AFTER),
+//            locals = CAPTURE_FAILSOFT)
+//    public void renderItemOverlayIntoGUI(FontRenderer renderer, ItemStack stack, int x, int y, String str, CallbackInfo ci, String s) {
+//        if (stack.getCount() > 64){
+//            s = endless$getSimplifiedCount(stack.getCount());
+//        }
+//    }
+
+    @ModifyVariable(method = "renderItemOverlayIntoGUI", at = @At("STORE"), ordinal = 1)
+    private String injected(String x) {
+        return endless$getSimplifiedCount(Integer.parseInt(x));
     }
 
     @Unique
