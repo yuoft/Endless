681d0e0af6c14af3cbc150040d705ab9786d27f1
yuo <2685245554@qq.com>
2025-07-03 22:19:31 +0800

    无尽流体修复

--- src/main/java/com/yuo/endless/Blocks/Fluid/InfinityFluidBlock.java
+++ src/main/java/com/yuo/endless/Blocks/Fluid/InfinityFluidBlock.java
@@ -7,10 +7,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.damagesource.DamageType;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -21,7 +17,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -72,12 +67,5 @@ public class InfinityFluidBlock extends LiquidBlock {
                 }
             }
         }
-    }
-
-    @Override
-    public boolean collisionExtendsVertically(BlockState state, BlockGetter level, BlockPos pos, Entity collidingEntity) {
-
-
-        return true;
     }
 }
